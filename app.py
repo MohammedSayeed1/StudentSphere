32b30179b6d3178f355b8e7688c249b9a6de041e
MohammedSayeed1 <145016554+MohammedSayeed1@users.noreply.github.com>
2025-01-03 19:08:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ nltk.download('opinion_lexicon')
 nltk_data_path = os.path.join(os.getcwd(), 'nltk_data')
 nltk.data.path.append(nltk_data_path)
 nltk.download('opinion_lexicon', download_dir=nltk_data_path)
+nltk.download('punkt', download_dir=nltk_data_path)
 
 app = Flask(__name__)
 app.config["SESSION_TYPE"] = "filesystem"
